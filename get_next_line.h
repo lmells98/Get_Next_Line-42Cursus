bcaf711e0498f1f7e59804c713a04441f241da6a
lmells98 <leighton.mells23@gmail.com>
2021-09-12 17:29:28 +0930

    Header Files

---
+++ get_next_line.h
@@ -0,0 +1,15 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_strchr(const char *s, const char c);
+char	*get_next_line(int fd);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_substr(const char *s, unsigned int start, size_t size);
+size_t	ft_strlen(const char *s);
+
+#endif
