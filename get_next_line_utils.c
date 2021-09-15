64fda8efdaedd7695e72e2924cd30cb665608cc6
lmells98 <leightn.mells23@gmail.com>
2021-09-15 16:53:53 +0930

    Utility Function for Get Next Line | Fix ft_strndup

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,7 +1,26 @@
 #include "get_next_line.h"
 
 //TODO	ADD FT_STRNDUP FUNCTION - DUPLICATE N_BYTES OF A STRING PASSED THROUGH
-char	*ft_strndup(const char *s, unsigned int size);
+char	*ft_strndup(const char *s, unsigned int size)
+{
+	char	*dup;
+	size_t	i;
+
+	dup = (char *)malloc(s[size + 1] * sizeof(char));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	if (i <= size)
+	{
+		while (s[i] != '\0')
+		{
+			dup[i] = s[i];
+			i++;
+		}
+		dup[i] = '\0';
+	}
+	return (dup);
+}
 
 size_t	ft_strlen(const char *s)
 {
