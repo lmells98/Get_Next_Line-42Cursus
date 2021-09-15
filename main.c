9a0815dce46904b9283a15d25d499af5e472f9ce
lmells98 <leightn.mells23@gmail.com>
2021-09-15 16:52:23 +0930

    MAIN FILE FOR TESTING

--- main.c
+++ main.c
@@ -3,7 +3,24 @@
 int main(void)
 {
 	int	fd;
+	char	*res;
 
-	fd = open("./text", O_RDONLY);
-	get_next_line(fd);
+	fd = open("test.txt", O_RDONLY);
+	printf("fd = %i\n", fd);
+	if (fd >= 0)
+	{
+		printf("SUCCESSFULL READ\n");
+		res = get_next_line(fd);
+		if (res)
+		{
+			printf("SUCCESSFUL PASS THROUGH FUNCTION\n");
+			printf("buffer = %s\n", res);
+		}
+		else
+		{
+			printf("FILE WAS NOT READ BY GET NEXT LINE\n");
+			return (0);
+		}
+	}
+	return (0);
 }
