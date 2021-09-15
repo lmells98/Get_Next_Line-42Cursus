00bd9b1a6d34112c51c1c6a5eddb517feff2c2fd
lmells98 <leightn.mells23@gmail.com>
2021-09-15 16:53:24 +0930

    HEADER FILE

--- get_next_line.h
+++ get_next_line.h
@@ -11,8 +11,7 @@
 # include <unistd.h>
 
 int		ft_strchr(const char *s, const char c);
-char	*ft_update_new_line(char **saved, int pos);
-char	*ft_out(char **save, int pos, int rd_bytes);
+char	*ft_strndup(const char *c, unsigned int size);
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
 
