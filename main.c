ede933a72a35cab8edae569a7df3388efc73ea76
lmells98 <leighton.mells23@gmail.com>
2021-09-16 19:54:42 +0930

    Main File for Testing!

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "get_next_line.h"
+
+int	main(void)
+{
+	int		fd;
+
+	fd = open("./text.txt", O_RDONLY);
+	if (fd < 0 || fd > 10240)
+	{
+		printf("ERROR! Failed to open file.");
+		return (0);
+	}
+	if (fd)
+		printf("\"%s\"\n", get_next_line(fd));
+	return (0);
+}
