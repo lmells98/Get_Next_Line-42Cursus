0c813cbb55f4471202ee36ac90944fde43fb33c2
lmells98 <leighton.mells23@gmail.com>
2021-09-16 16:13:15 +0930

    get_next_line.c | Added basic functionallity | Need to Write a main to test File input

--- get_next_line.c
+++ get_next_line.c
@@ -4,14 +4,22 @@ char	*get_next_line(int fd)
 {
 	int			pos;
 	int			rd_bytes;
-	int			len;
-	char		*buffer;
+	char		*buf;
+	static char	*saved;
 
 	if (BUFFER_SIZE == 0 || fd < 0 || fd > 10240)
 		return (NULL);
-	buffer = NULL;
-	rd_bytes = read(fd, buffer, BUFFER_SIZE);
-	pos = ft_strchr(buffer, '\n');
-	len = ft_strlen(buffer);
-	return (buffer);
+	buf = NULL;
+	pos = ft_strchr(saved, '\n', 0);
+	while (pos == -1 && pos != -5)
+	{
+		buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+		if (!buf)
+			return (NULL);
+		rd_bytes = read(fd, buf, BUFFER_SIZE);
+		if (rd_bytes == -1 || rd_bytes == 0)
+			return ;
+		utils_debug(fd, line, rd_bytes);
+	}
+	return (NULL);
 }
