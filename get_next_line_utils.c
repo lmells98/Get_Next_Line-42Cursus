06f2e39718e68c89409c184fc7ff5b1cba937d7b
lmells98 <leightn.mells23@gmail.com>
2021-09-17 15:11:29 +0930

    get_next_line_utils.c | Utility Functions for get_next_line

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -54,3 +54,35 @@ int	ft_strchr(char *str, int c, int flag)
 
 /*	TODO	Add ft_strnjoin FUNCTION TO JOIN THE EXISTING
 **	STRING TO THE NEW LINE IN BUFFER */
+char	*ft_strnjoin(char *s1, char *s2, int n)
+{
+	char	*join;
+	int		i;
+	int		j;
+
+	j = 0;
+	i = 0;
+	if (s2[j] == '\0')
+		return (NULL);
+	join = (char *)malloc((ft_strlen(s1) + n + 1) * sizeof(char));
+	if (!join)
+		return (NULL);
+	if (s1 != NULL)
+	{
+		while (s1[i] != '\0')
+		{
+			join[i] = s1[i];
+			i++;
+		}
+	}
+	while (s2[i] != '\0' && j < n)
+	{
+		join[i] = s2[j];
+		i++;
+		j++;
+	}
+	join[i] = '\0';
+	if (s1)
+		ft_free(&s1);
+	return (join);
+}
