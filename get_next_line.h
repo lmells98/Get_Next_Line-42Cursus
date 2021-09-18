a7b7628035f48ba9a4ea582cda08447e4f5f1796
lmells98 <leighton.mells23@gmail.com>
2021-09-18 13:34:08 +0930

    get_next_line.h | Need to rework before submission

--- get_next_line.h
+++ get_next_line.h
@@ -11,19 +11,19 @@
 # include <unistd.h>
 
 int		ft_strchr(char *str, int c, int flag);
+int		ft_strlen(char *s);
 char	*get_next_line(int fd);
 char	*ft_strndup(const char *s, unsigned int size);
 char	*ft_strnjoin(char *s1, char *s2, int n);
 void	ft_free(char **pointer);
 void    *ft_calloc(size_t n_memb, size_t size);
-size_t	ft_strlen(const char *s);
 
-void	debug_info(int fd, char *buf, int buf_size, int bytes);
+void	debug_info(int fd, char *buf, int buf_size, int rd_bytes, char *saved);
 void	utils_debug(int fd, char *buf, int bytes, char *saved);
 
 int		debug_strchr(char *str, char c, int flag);
 void	debug_len(char *str);
 void	debug_strndup(char *src, unsigned int size);
-void	debug_strnjoin(char *s1, char *s2, int pos);
+void	debug_strnjoin(char *s1, char *s2, int n);
 
 #endif
