a1d783ca7a2fafdc4e171f7444dfda9aaab113b6
lmells98 <leightn.mells23@gmail.com>
2021-09-20 13:01:14 +0930

    get_next_line_bonus.c | BONUS

--- bonus/get_next_line_bonus.c
+++ bonus/get_next_line_bonus.c
@@ -1,5 +1,25 @@
 #include "get_next_line_bonus.h"
 
+void	*ft_calloc(size_t n_memb, size_t size)
+{
+	char			*ch_p;
+	size_t			bytes;
+	unsigned int	i;
+
+	bytes = n_memb * size;
+	ch_p = (char *)malloc(bytes);
+	i = 0;
+	if (ch_p == NULL)
+		return (0);
+	while (i < bytes)
+	{
+		ch_p[i] = 0;
+		i++;
+	}
+	printf("Pointer=%s\n", ch_p);
+	return (ch_p);
+}
+
 char	*get_next_line(int fd)
 {
 	static char	*saved[10240];
@@ -11,8 +31,20 @@ char	*get_next_line(int fd)
 		return (NULL);
 	buf = NULL;
 	pos = ft_strchr(saved[fd], '\n', 0);
-	rd_bytes = read(fd, buf, BUFFER_SIZE);
+	while (pos == -1 && pos != -5)
+	{
+		buf = ft_calloc(BUFFER_SIZE + 1, 1);
+		if (buf == NULL)
+			return (NULL);
+		rd_bytes = read(fd, buf, BUFFER_SIZE);
+		if (rd_bytes == 0 || rd_bytes == -1)
+			return (NULL);
+		saved[fd] = ft_strnjoin(saved[fd], buf, rd_bytes);
+		pos = ft_strchr(saved[fd], '\n', 1);
+	}
 	printf("--------\nbuf=%s\n--------\n", buf);
+	printf("--------\nsaved=%s\n--------\n", saved[fd]);
 	printf("pos=%i\n-------\nbytes=%i\n---------\n", pos, rd_bytes);
-	return (0);
+	ft_free(&buf);
+	return (buf);
 }
