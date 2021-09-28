215ca7e6bb7fe0e83a2e8c35f9129e9d37d0cae3
lmells98 <leightn.mells23@gmail.com>
2021-09-28 14:22:46 +0930

    TESTING FILES

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lmells <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 10:59:42 by lmells            #+#    #+#             */
-/*   Updated: 2021/09/20 10:59:42 by lmells           ###   ########.fr       */
+/*   Updated: 2021/09/28 11:34:30 by lmells           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,12 @@ int	main(void)
 	int		i;
 
 	fd = open("./text.txt", O_RDONLY);
-	if (fd < 0 || fd > 10240)
+	if (fd > -1 && fd < 10241)
 	{
+		for (i = 1; i <= 4; i++)
+			printf("%i:	%s", i, get_next_line(fd));
+	}
+	else
 		printf("ERROR! Failed to open file.");
-		return (0);
-	}
-	for (i = 1; i <= 12; i++)
-	{
-		printf("%i:	%s", i, get_next_line(fd));
-	}
 	return (0);
 }
