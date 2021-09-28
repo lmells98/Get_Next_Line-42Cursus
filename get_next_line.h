f3d6b7962f6e3a4374f67e82e2a2ddc78ac43a46
lmells98 <leightn.mells23@gmail.com>
2021-09-28 13:52:11 +0930

    get_next_line.h Mandatory

--- get_next_line.h
+++ get_next_line.h
@@ -3,7 +3,6 @@
 
 # include <fcntl.h>
 # include <stdlib.h>
-# include <stdio.h>
 # include <unistd.h>
 
 int		ft_strchr(char *str, int c, int flag);
@@ -12,6 +11,6 @@ char	*get_next_line(int fd);
 char	*ft_strndup(const char *s, unsigned int size);
 char	*ft_strnjoin(char *s1, char *s2, int n);
 void	ft_free(char **pointer);
-void    *ft_calloc(size_t n_memb, size_t size);
+void	*ft_calloc(size_t n_memb, size_t size);
 
 #endif
